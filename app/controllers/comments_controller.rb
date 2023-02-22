d06b78ba0c07f58813e52501df24c416173bfbbd
atsushi0610 <kogure.bbc@gmail.com>
2023-02-22 16:42:32 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+def create
+  @comment = Comment.new(commnet_params)
+  if @comment.save
+    redirect_to prototype_path(@comment.prototype)
+  else
+    @prototype = Prototype.find(params[:id])
+    @comments = @prototype.comments
+    render "prototypes/show"
+  end
+end
+
+private
+  def commnet_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
+
